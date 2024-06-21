f218d2233d13b07692728538b201413d9b4dae5f
andryapril01 <165459563+andryapril01@users.noreply.github.com>
2024-06-21 16:12:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="wrapper">
+        <?php include "layout/header.html"?>
+
+        <main>
+             <p class="text-center">Halo Selamat Datang  di Website Kami</p>
+             <div class="image-gallery">
+                <img src="gambar6.jpg" alt"gambar 1">
+                <img src="bahan 2.jpg" alt"gambar 2">
+                <img src="bahan 33.jpg" alt"gambar 3">
+            </div>
+        </main>
+    
+     <?php include "layout/footer.html"?>
+    </div>
+</body>
+</html>
